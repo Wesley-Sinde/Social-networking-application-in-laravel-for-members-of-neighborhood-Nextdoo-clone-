77d5327eb23af0131f65c03a82b2672d25f614eb
Wesley Sinde <sindewesley5@gmail.com>
2022-06-03 10:14:26 +0300

    Signed-off-by: Wesley Sinde <sindewesley5@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -301,7 +301,7 @@
 
                             <li>
                                 <a href="{{ url('/') }}"
-                                    class="block py-2 pl-3 pr-4  rounded md:bg-transparent  md:p-0 dark:text-white  {{ request()->segment(1) == '' ? 'text-blue-700 hover:text-blue-700 dark:text-blue-500' : '' }}"
+                                    class="block py-2 pl-3 pr-4  rounded md:bg-transparent  md:p-0 dark:text-white  {{ request()->segment(1) == 'home' ? 'text-blue-700 hover:text-blue-700 dark:text-blue-500' : '' }}"
                                     aria-current="page">Home</a>
                             </li>
                             <li>
@@ -326,8 +326,9 @@
                                 <div
                                     class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
 
-                                    <button id="theme-toggle" data-tooltip-target="tooltip-toggle" type="button"
-                                        class="text-sm text-gray-500 rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
+
+                                    <button id="theme-toggle" type="button"
+                                        class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm mx-2">
                                         <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor"
                                             viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z">
@@ -340,6 +341,22 @@
                                                 fill-rule="evenodd" clip-rule="evenodd"></path>
                                         </svg>
                                     </button>
+
+
+                                    {{-- <button id="theme-toggle" data-tooltip-target="tooltip-toggle" type="button"
+                                        class="text-sm text-gray-500 rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">
+                                        <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor"
+                                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z">
+                                            </path>
+                                        </svg>
+                                        <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor"
+                                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                                fill-rule="evenodd" clip-rule="evenodd"></path>
+                                        </svg>
+                                    </button> --}}
                                     <div id="tooltip-toggle" role="tooltip"
                                         class="absolute z-10 invisible inline-block px-3 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
                                         Toggle dark mode
