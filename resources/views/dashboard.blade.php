dccd16c0415f72bac57245603e828ac2ea4982b7
Wesley Sinde <sindewesley5@gmail.com>
2022-06-07 22:57:52 +0300

    Signed-off-by: Wesley Sinde <sindewesley5@gmail.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -158,7 +158,7 @@
                                 </svg>
                             </div>
                             <div class="ml-3 text-sm font-normal"> {{ session()->get('message') }}</div>
-                            <button type="button"
+                            <button type="button" 
                                 class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
                                 data-collapse-toggle="toast-success" aria-label="Close">
                                 <span class="sr-only">Close</span>
